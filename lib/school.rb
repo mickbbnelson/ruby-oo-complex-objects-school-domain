6a11e29379d2421ae8370c616ac4b29304903284
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-04-17 11:38:38 -0700

    looking back as a refresher

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,4 @@
+
 require "pry"
 
 # code here!
@@ -10,9 +11,8 @@ class School
     end
 
     def add_student(name_value, grade_key)
-        
-        @roster[grade_key] ||= []
-        @roster[grade_key] << name_value
+        @roster[grade_key] ||= []      #if @roster grade_key exists, keep and use the grade_key.  If not, set a new hash up with that garde_key
+        @roster[grade_key] << name_value  #Shovel in the name_vale into the hash value which is an array
     end
     def grade(grade)
         @roster[grade]
